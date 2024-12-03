c1b4c0a8c8f601f03289c4a2b1a187a6ddbefbd5
Akshai Krishna S <akshai404akz@gmail.com>
2024-12-03 18:27:29 +0530

    Update SocketContext to use local server URL for socket connection

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -9,7 +9,7 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null)
 
   useEffect(() => {
-    setSocket(io('https://full-stack-estate-7zs3.onrender.com'))
+    setSocket(io('http://localhost:4000'))
   }, [])
 
   useEffect(() => {
