0d4a36a9c4791feef486fe60ea40f46c16b3bfcc
Akshai Krishna S <akshai404akz@gmail.com>
2024-12-06 19:48:07 +0530

    dd

--- api/app.js
+++ api/app.js
@@ -12,12 +12,14 @@ import adminRoute from './routes/admin.route.js'
 const app = express()
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', req.headers.origin || '*')
-  res.header('Access-Control-Allow-Credentials', 'true')
-  res.header('Access-Control-Allow-Methods', '*')
-  res.header('Access-Control-Allow-Headers', '*')
+  res.setHeader('Access-Control-Allow-Origin', 'https://full-stack-estate.vercel.app')
+  res.setHeader('Access-Control-Allow-Methods', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+
   if (req.method === 'OPTIONS') {
-    return res.status(200).send()
+    res.sendStatus(200)
+    return
   }
   next()
 })
