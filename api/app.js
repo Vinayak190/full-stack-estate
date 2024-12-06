1fd9829606fb564a3e453d4b2c9f1ee7faab569b
Akshai Krishna S <akshai404akz@gmail.com>
2024-12-06 19:55:08 +0530

    ef

--- api/app.js
+++ api/app.js
@@ -13,12 +13,12 @@ const app = express()
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://full-stack-estate.vercel.app')
-  res.setHeader('Access-Control-Allow-Methods', '*')
-  res.setHeader('Access-Control-Allow-Headers', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
   res.setHeader('Access-Control-Allow-Credentials', 'true')
 
   if (req.method === 'OPTIONS') {
-    res.sendStatus(200)
+    res.status(204).end()
     return
   }
   next()
